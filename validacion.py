5890d27b51315c7632b8791e867bbe92392db986
Randall_Acosta <randallacosta53@gmail.com>
2026-04-20 23:18:27 -0600

    Update validacion.py

--- validacion.py
+++ validacion.py
@@ -1,11 +1,5 @@
-#Randall José Acosta Navarro
-#Summy Vanessa Reyes Conejo
-
 def validar_numero(entrada):
     while not entrada.isdigit():
         print("Por favor, ingrese un número válido.")
         entrada = input()
     return int(entrada)
-
-#Randall José Acosta Navarro
-#Summy Vanessa Reyes Conejo
